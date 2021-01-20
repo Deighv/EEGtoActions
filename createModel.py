83c9da7e15cd22d091d691d72d908a79b83bd946
deighv <David.Dennehey@nau.edu>
2021-01-20 16:06:09 -0700

    tinkering

--- createModel.py
+++ createModel.py
@@ -2,11 +2,20 @@ import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 import psycopg2
+
+class myCallback(tf.keras.callbacks.Callback):
+  def on_epoch_end(self, epoch, logs={}):
+    if(logs.get('accuracy')>=0.90):
+      measAcc = str(round(logs.get("accuracy")*100,2))
+      print("\nReached " + measAcc + "% accuracy, stopping training")
+      self.model.stop_training = True
+callbacks = myCallback()
+
 print("Connecting to Database")
 conn = psycopg2.connect(database="eeg", user="postgres", password="penislol", host="127.0.0.1", port="5432")
 print(conn)
-#Get newest 1000 controller index states
 
+#Get 1000 latest controller indexes
 controllerIndexSQL = """
 SELECT cpi.controller_press_index_id
 	FROM controller_data_normalized_view vw
@@ -21,6 +30,8 @@ RIGHT OUTER JOIN controller_press_index cpi ON
   vw.b = cpi.b
 ORDER BY Time_ID ASC LIMIT 1000;
 """
+
+#Get all rows - first 1000
 controllerTrainingIndexSQL = """
 SELECT cpi.controller_press_index_id
 	FROM controller_data_normalized_view vw
@@ -36,29 +47,7 @@ RIGHT OUTER JOIN controller_press_index cpi ON
 ORDER BY Time_ID ASC 
 LIMIT (SELECT COUNT(controller_data_normalized_view.x) FROM controller_data_normalized_view) - 1000 offset 1000;
 """
-eegTrainingDataSQL = """
-SELECT  
-ROUND(SUM(GREATEST(channel0, 0)),0),
-ROUND(SUM(GREATEST(channel1, 0)),0),
-ROUND(SUM(GREATEST(channel2, 0)),0),
-ROUND(SUM(GREATEST(channel3, 0)),0),
-ROUND(SUM(GREATEST(channel4, 0)),0),
-ROUND(SUM(GREATEST(channel5, 0)),0),
-ROUND(SUM(GREATEST(channel6, 0)),0),
-ROUND(SUM(GREATEST(channel7, 0)),0),
-ROUND(SUM(GREATEST(channel8, 0)),0),
-ROUND(SUM(GREATEST(channel9, 0)),0),
-ROUND(SUM(GREATEST(channel10, 0)),0),
-ROUND(SUM(GREATEST(channel11, 0)),0),
-ROUND(SUM(GREATEST(channel12, 0)),0),
-ROUND(SUM(GREATEST(channel13, 0)),0),
-ROUND(SUM(GREATEST(channel14, 0)),0),
-ROUND(SUM(GREATEST(channel15, 0)),0)
-  FROM headset_data 
-GROUP BY Time_ID 
-ORDER BY Time_ID ASC
-LIMIT (SELECT COUNT(headset_data.channel0) FROM headset_data) - 1000 offset 1000;
-"""
+
 #Get newest 1000 eeg states
 EEGSql = """
 SELECT  
@@ -83,6 +72,32 @@ GROUP BY Time_ID
 ORDER BY Time_ID ASC
 LIMIT 1000;
 """
+
+#Get all EEG data rows excluding the first 1000
+eegTrainingDataSQL = """
+SELECT  
+ROUND(SUM(GREATEST(channel0, 0)),0),
+ROUND(SUM(GREATEST(channel1, 0)),0),
+ROUND(SUM(GREATEST(channel2, 0)),0),
+ROUND(SUM(GREATEST(channel3, 0)),0),
+ROUND(SUM(GREATEST(channel4, 0)),0),
+ROUND(SUM(GREATEST(channel5, 0)),0),
+ROUND(SUM(GREATEST(channel6, 0)),0),
+ROUND(SUM(GREATEST(channel7, 0)),0),
+ROUND(SUM(GREATEST(channel8, 0)),0),
+ROUND(SUM(GREATEST(channel9, 0)),0),
+ROUND(SUM(GREATEST(channel10, 0)),0),
+ROUND(SUM(GREATEST(channel11, 0)),0),
+ROUND(SUM(GREATEST(channel12, 0)),0),
+ROUND(SUM(GREATEST(channel13, 0)),0),
+ROUND(SUM(GREATEST(channel14, 0)),0),
+ROUND(SUM(GREATEST(channel15, 0)),0)
+  FROM headset_data 
+GROUP BY Time_ID 
+ORDER BY Time_ID ASC
+LIMIT (SELECT COUNT(headset_data.channel0) FROM headset_data) - 1000 offset 1000;
+"""
+
 with conn.cursor() as curs:
     curs.execute(controllerIndexSQL)
     Controller_State = curs.fetchall()
@@ -119,13 +134,28 @@ EEG_Training_State = np.asarray(EEG_Training_State)
 EEG_Training_State = EEG_Training_State.astype(int)
 print("EEG Training Data Numpy'd")
 
+
+
+#need to figure out how to reshape the data to better prepare it for modeling
+#Reshape, 60k is for number of images, 28 is width/height, 1 is color (actually 0-255 divided by 255)
+#1413885 number of rows in db-1000 for testing
+#16 is number of channels
+#1 is dimensions in each array (just a row)
+#Final number is the range of eeg data after dividing by highest number
+#could normalize this to be in scale with each channel not by overall highest
+#I have no idea what I'm doing
+EEG_Training_State=EEG_Training_State.reshape(1413885, 16, 1, 1)
+EEG_Training_State=EEG_Training_State / 68645
+EEG_State = EEG_State.reshape(1000, 16, 1, 1)
+EEG_State=EEG_State/ 68645
+
 model = tf.keras.models.Sequential([
     keras.layers.Flatten(),
-    keras.layers.Dense(128, activation=tf.nn.relu),
+    keras.layers.Dense(192, activation=tf.nn.relu),
     keras.layers.Dense(81, activation=tf.nn.softmax)
 ])
 
-print("model created")
+print("compiling model..")
 model.compile(optimizer = tf.keras.optimizers.Adam(),
               loss = 'sparse_categorical_crossentropy',
               metrics=['accuracy'])
@@ -133,7 +163,7 @@ print("model compiled, attempting fit with data")
 ###
 #Grab new eeg_state+controller_state to test model with
 ###
-model.fit(EEG_Training_State, Controller_Training_State, epochs=5)
+model.fit(EEG_Training_State, Controller_Training_State, epochs=5000, callbacks=[callbacks])
 
 model.evaluate(EEG_State, Controller_State)
 model.save("mymodel")
